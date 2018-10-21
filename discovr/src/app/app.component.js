2f0678777211d497f354020e1778edb4c99249a7
VSell <valeria1897@gmail.com>
2018-10-20 21:42:20 -0600

    fixmain

--- discovr/src/app/app.component.js
+++ discovr/src/app/app.component.js
@@ -12,9 +12,10 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
+import { Main } from '../pages/main/main';
 var MyApp = /** @class */ (function () {
     function MyApp(platform, statusBar, splashScreen) {
-        this.rootPage = HomePage;
+        this.rootPage = Main;
         platform.ready().then(function () {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
